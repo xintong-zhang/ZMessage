a95edad1916da77ad995e2088d780c11f7dfb174
xintong-zhang <xz@zestfinance.com>
2013-03-12 09:16:11 -0700

    add-rspec

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,8 @@
 class Message < ActiveRecord::Base
   attr_accessible :content, :phone_number
+  validates :phone_number, presence: true
+  validates :content, presence: true
+  validates :phone_number, length: {:is => 10}, :numericality => {:only_integer => true}  
+  validates :content, :length=> {:maximum => 50}
+
 end
